b9aea5b3480e01e54ce46dfe7342cb34f017f8c1
Kathleen Howard <kmaryhoward@gmail.com>
2019-03-02 18:14:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
+end
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -18,31 +20,45 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
 
-end
-
+  def add_supply_to_winter_holidays(holiday_hash, supply)
+    holiday_hash.map do |key, value|
+      if key == :winter
+        value.map do |key, value|
+          value.push(supply)
+          end
+        end
+      end
+    holiday_hash
+  end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash.map do |seasons, holiday|
+    if seasons == season
+      holiday.merge! ({ holiday_name => supply_array })
+    end
+  end
+  holiday_hash
 end
 
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
 
+def all_winter_holiday_supplies(holiday_hash)
+  a = []
+  holiday_hash.each do |key, value|
+    if key == :winter
+      value.each do |key, value|
+        value.each do |item|
+          a << item
+        end
+      end
+    end
+  end
+  a
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -61,10 +77,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
